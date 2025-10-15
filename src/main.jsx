df10efa099677d80baaaf072b37c42afeffae601
Krish Anjara <127621879+KrishAnjara@users.noreply.github.com>
2025-10-15 19:54:16 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
 
+import SpeedInsights from '@vercel/speed-insights/next'
+import Analytics from "@vercel/analytics/next"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>,
 )
