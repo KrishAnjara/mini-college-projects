01e4478fa240ce714b825dd46179ba095be842aa
Krish Anjara <127621879+KrishAnjara@users.noreply.github.com>
2025-10-15 20:20:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/index.css';
-import { injectSpeedInsights } from '@vercel/speed-insights';
 import { inject } from '@vercel/analytics';
 
-injectSpeedInsights();
 inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
